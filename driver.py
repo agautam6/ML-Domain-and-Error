3585732d7776908d19d2e4c95fe8202e5881941c
anupraas <agautam6@wisc.edu>
2020-02-02 12:22:40 -0600

    Adding test2 - training with 'alldata' and testing with 'Pd_only' or 'no_Pd'

--- driver.py
+++ driver.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from package import gpr
 from package import rf
+from tabulate import tabulate
 
 
 def plot(res, sigma, model_name, number_of_bins):
@@ -94,7 +95,20 @@ def getdata(data):
     return data
 
 
-def main():
+# Fetch data with filename
+def importdatanames(filename):
+    data = pd.read_csv(filename, header=None, sep=',')
+    return data
+
+
+def predictdomain(GPR_error, RF_error):
+    if GPR_error < 0.8 and RF_error < 0.8:
+       return 1
+    else:
+       return 0
+
+
+def test1():
     data = importdata('_haijinlogfeaturesnobarrier_alldata.csv')
     data = getdata(data)
     X_train, X_test, y_train, y_test = train_test_split(data.iloc[:,:-1], data.iloc[:,-1], test_size=0.3)
@@ -106,6 +120,58 @@ def main():
     RF.train(X_train, y_train)
     res, sigma = RF.getrfmetrics(X_test, y_test)
     plot(res, sigma, "RF", 8)
+
+
+def test2():
+    data = importdata('_haijinlogfeaturesnobarrier_alldata_no_Pd.csv')
+    data = getdata(data)
+    # X_train, X_test, y_train, y_test = train_test_split(data.iloc[:,:-1], data.iloc[:,-1], test_size=0)
+    X_train = data.iloc[:,:-1]
+    y_train = data.iloc[:,-1]
+    GPR = gpr.GPR()
+    GPR.train(X_train, y_train)
+    test_data = importdata('_haijinlogfeatures_Pd_only.csv')
+    test_data = test_data.drop([24,25], axis=1)
+    test_data = test_data.drop([0])
+    # Define GPR and RF errors
+    pred, GPR_errors = GPR.predict(test_data, True)
+    RF = rf.RF()
+    RF.train(X_train, y_train)
+    pred, RF_errors = RF.predict(test_data, True)
+
+    # GPR_errors = np.random.random_sample(37, )
+    # RF_errors = np.random.random_sample(37, )
+
+    data = importdatanames('_haijinlogfeatures_Pd_only.csv')
+
+    list = []
+    for element in data[24]:
+        list.append(element)
+    del list[0]
+
+    list2 = []
+    for element in data[25]:
+        list2.append(element)
+    del list2[0]
+
+    final_list = []
+    for i in range(0, 37):
+        first = list[i]
+        second = list2[i]
+        combined = first + "-" + second
+        final_list.append(combined)
+    print(final_list)
+
+    predictions = [predictdomain(GPR_errors[i], RF_errors[i]) for i in range(0, 37)]
+
+    results = [(final_list[i], predictions[i], GPR_errors[i], RF_errors[i]) for i in range(0, 37)]
+
+    print(tabulate(results, headers=["Material", "In domain?", "GPR predicted error", "RF predicted error"]))
+
+
+def main():
+    # test1()
+    test2()
 
 
 if __name__ == "__main__":
