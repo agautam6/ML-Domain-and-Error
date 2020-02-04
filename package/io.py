e746d08c4a172cbf1ae7c1ff8023fe05aa24d1f9
anupraas <agautam6@wisc.edu>
2020-02-04 13:33:07 -0600

    Update io.py

--- package/io.py
+++ package/io.py
@@ -12,6 +12,7 @@ def importdata(filename):
 # Sanitize data. Temporarily dropping columns E_regression, Material Composition
 def sanitizedata(data):
     col_list = ['Material compositions 1', 'Material compositions 2', 'E_regression']
+    # Remove columns if present
     for c in col_list:
         if c in data.columns:
             data = data.drop(columns=[c])
