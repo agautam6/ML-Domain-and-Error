29999fac9f85b1407e766dcf36cc37aaa8b8015a
anupraas <agautam6@wisc.edu>
2020-02-09 17:53:27 -0600

    Update sanitizedata() to accept list of additional columns to be dropped

--- package/io.py
+++ package/io.py
@@ -10,9 +10,11 @@ def importdata(filename):
 
 
 # Sanitize data. Temporarily dropping columns E_regression, Material Composition
-def sanitizedata(data):
+def sanitizedata(data, user_list=None):
     col_list = ['Material compositions 1', 'Material compositions 2', 'E_regression', 'predict_Pt',
                 'Hop activation barrier']
+    if user_list is not None:
+        col_list = col_list + user_list
     # Remove columns if present
     for c in col_list:
         if c in data.columns:
