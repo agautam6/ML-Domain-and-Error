70c127ea24ddd1c5c1ba135eb9fe3d71739e6742
anupraas <agautam6@wisc.edu>
2020-02-19 15:12:55 -0600

    Using model save/load from io package

--- test_PVdomain.py
+++ test_PVdomain.py
@@ -1,21 +1,11 @@
-import pickle
-from datetime import datetime as dt
-import pytz as tz
 import statistics
 from tabulate import tabulate
-from package import gpr
-from package import io
-from package import rf
-from package import testhelper as th
+from package import gpr, io, rf, testhelper as th
 
-
-def getModelSaveFileName(filename):
-    return filename.replace("/", "_").replace(".", "_") + "_" \
-           + dt.now(tz=tz.timezone('America/Chicago')).strftime("%m-%d-%y_%H-%M-%S")
-
-
-gpr_savedmodel = None
-rf_savedmodel = None
+gpr_savedmodel = 'models/GPR_data_PVstability_Weipaper_alldata_featureselected_csv_02-18-20_22-26-49'
+rf_savedmodel = 'models/RF_data_PVstability_Weipaper_alldata_featureselected_csv_02-18-20_22-26-54'
+# gpr_savedmodel = None
+# rf_savedmodel = None
 rfslope = 0.89066261
 trainfile = 'data/PVstability_Weipaper_alldata_featureselected.csv'
 testfile = 'data/PVstability_Weipaper_testdata_featureselected.csv'
@@ -36,20 +26,16 @@ test_data = io.sanitizedata(test_data, user_list=['Material Composition', 'group
 if gpr_savedmodel is None:
     GPR = gpr.GPR()
     GPR.train(X_train, y_train, std=y_std)
-    with open('GPR_' + getModelSaveFileName(trainfile) + '.pkl', 'wb') as out:
-        pickle.dump(GPR, out, pickle.HIGHEST_PROTOCOL)
+    io.savemodelobj(GPR, 'GPR_' + trainfile)
 else:
-    with open(gpr_savedmodel, 'rb') as inp:
-        GPR = pickle.load(inp)
+    GPR = io.loadmodelobj(gpr_savedmodel)
 
 if rf_savedmodel is None:
     RF = rf.RF()
     RF.train(X_train, y_train, std=y_std)
-    with open('RF_' + getModelSaveFileName(trainfile) + '.pkl', 'wb') as out:
-        pickle.dump(RF, out, pickle.HIGHEST_PROTOCOL)
+    io.savemodelobj(GPR, 'RF_' + trainfile)
 else:
-    with open(rf_savedmodel, 'rb') as inp:
-        RF = pickle.load(inp)
+    RF = io.loadmodelobj(rf_savedmodel)
 
 gpr_pred, GPR_errors = GPR.predict(test_data, True)
 rf_pred, RF_errors = RF.predict(test_data, True)
