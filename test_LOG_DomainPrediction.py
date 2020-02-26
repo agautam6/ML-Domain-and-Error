e454463de0a850cd92e1323f8f9f7816892d9a34
GRISHMA GUPTA <60323683+grishma31@users.noreply.github.com>
2020-02-26 10:37:36 -0600

    Domain prediction analysis for LOG Test

--- test_LOG_DomainPrediction.py
+++ test_LOG_DomainPrediction.py
@@ -1,4 +1,7 @@
 # generate plots for Diffusion dataset - LOG for RF
+import statistics
+
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import LeaveOneGroupOut
 from tabulate import tabulate
@@ -15,13 +18,15 @@ Y = data.iloc[:, 0]
 rf_res = np.asarray([])
 rf_sigma = np.asarray([])
 rfslope = 1.143641
+y_std = statistics.stdev(Y.to_numpy(dtype=float))
 
 # Leave out group test for Material compositions 1
 rfk = LeaveOneGroupOut()
 j = 0
 first = 1
 fd = open('domain results/Diffusion_DataSet_LOG__domain_results.txt', 'a+')
-table = []
+in_domain = []
+out_domain = [[], [], []]
 
 pdtabulate = lambda df: tabulate(df, headers=["Material", "In domain?", "GPR predicted error",
                                               str(rfslope) + " * RF predicted Error"], tablefmt='psql')
@@ -45,5 +50,25 @@ for train_index, test_index in rfk.split(X, Y, groups):
                range(0, len(X_test))]
     j = j + len(X_test)
 
+    for i in range(0, len(X_test)):
+        residual_by_std = abs(rf_pred[i] - y_test.to_numpy(dtype=float)[i]) / y_std
+        predicted_error = RF_errors[i]
+        if predictions[i] is 1:
+            in_domain.append(residual_by_std / predicted_error)
+        else:
+            out_domain[th.getcontribution(GPR_errors[i], RF_errors[i]) - 1].append(residual_by_std / predicted_error)
+
     print(pdtabulate(results), file=fd)
+
 fd.close()
+plt.hist(in_domain)
+plt.ylabel('Counts')
+plt.xlabel('RF absolute residual / RF predicted error')
+plt.savefig("in_domain_Diffusion_DataSet_LOG__domain_results.png")
+plt.clf()
+plt.hist(out_domain, stacked=True, label=['GPR', 'RF', 'both'])
+plt.legend(prop={'size': 10})
+plt.ylabel('Counts')
+plt.xlabel('RF absolute residual / RF predicted error')
+plt.savefig("out_domain_Diffusion_DataSet_LOG__domain_results.png")
+plt.clf()
